e4e7c61f684601d307f623945211cd259e3a9460
ankitjavalkar <ankitjavalkar@users.noreply.github.com>
2017-04-17 15:35:55 +0530

    Add mail sending script

---
+++ mail_sender/credentials.py
@@ -0,0 +1,10 @@
+"""
+credentials.py
+
+Stores the username, password and smtp server URL of the email account used
+by send_mail.py as strings
+"""
+
+username = 'senders_username'
+password = 'senders_password'
+server_url = 'smtp_url' # Use 'smtp.gmail.com:587' for gmail
---
+++ mail_sender/send_mail.py
@@ -0,0 +1,81 @@
+import sys
+import smtplib
+import csv
+
+from credentials import username, password, server_url
+
+
+class IncorrectValueError(Exception):
+    pass
+
+
+# Add Sender's email corresponding to the SMTP specified in credentials.py
+FROM_ADDR = 'senders_email'
+
+# Add subject line of email
+SUB = 'subject_line'
+
+# Add message body. No need to add salutations like Dear/Hello etc.
+MSG_BODY = ('Sample Message:'
+        'This mail has been sent by a Robot\n'
+        'that is going to take over the world\n'
+        '-----------\n'
+        'This script can now send a mail to anyone\n'
+        'you want from your email account'
+    )
+
+
+def get_all_to_addr(csv_file):
+    to_addr_list = []
+    with open(csv_file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            name = row['name']
+            email = row['email']
+            to_addr_list.append((name.strip(), email.strip()))
+    return to_addr_list
+
+
+def set_message(from_addr, to_addr, subject, name, msg_body):
+    if not from_addr:
+        raise IncorrectValueError('The senders address is not provided')
+    if not to_addr:
+        raise IncorrectValueError('The recipients address is not provided')
+    if not msg_body:
+        raise IncorrectValueError('The message body is not provided')
+
+    salutation = "Dear {0}".format(name)
+    msg = "From: {0}\nTo: {1}\nSubject: {2}\n\n{3}\n{4}""".format(from_addr,
+        ", ".join(to_addr), subject, salutation, msg_body)
+    return msg
+
+
+def send_mail(from_addr, to_addr, subject, message):
+    # The actual mail send
+    try:
+        server = smtplib.SMTP(server_url)
+        server.ehlo()
+        server.starttls()
+        server.login(username,password)
+        server.sendmail(from_addr, to_addr, message)
+        server.quit()
+    except Exception as e:
+        print e
+        with open('errors.csv', 'wb') as csvfile:
+            spamwriter = csv.writer(csvfile)
+            spamwriter.writerow([from_addr, to_addr[0], e.message])
+        sys.exit(1)
+
+
+def main():
+    from_addr = FROM_ADDR
+    subject = SUB
+    msg_body = MSG_BODY
+    csv_file = sys.argv[1]
+    to_addr_list = get_all_to_addr(csv_file)
+    for name, to_addr in to_addr_list:
+        message = set_message(from_addr, [to_addr], subject, name, msg_body)
+        send_mail(from_addr, [to_addr], subject, message)
+
+if __name__ == '__main__':
+    main()
